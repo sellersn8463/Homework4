9f9997464f02ccb425085c96bd05880a30d223ca
Nathan Sellers <32393890+sellersn8463@users.noreply.github.com>
2021-04-25 12:48:53 -0500

    Add files via upload

---
+++ homework-4-whitebox-testing-sellersn8463-master/KSU.CIS.640.VendingMachine/KSU.CIS.640.VendingMachine/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public static class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+        private static void Run()
+        {
+            VendingMachine vend = new VendingMachine();
+            while (vend.Status != VendingMachine.State.OFF)
+            {
+                if (vend.Status == VendingMachine.State.START)
+                {
+                    vend.Start();
+                }
+                if (vend.Status == VendingMachine.State.INSERT)
+                {
+                    vend.InsertMoney();
+                }
+                if (vend.Status == VendingMachine.State.SELECT)
+                {
+                    vend.Select();
+                }
+                if (vend.Status == VendingMachine.State.DISPENSE)
+                {
+                    vend.DispenseSelection();
+                }
+                if (vend.Status == VendingMachine.State.CHANGE)
+                {
+                    vend.DispenseChange();
+                }
+                if (vend.Status == VendingMachine.State.STOCK)
+                {
+                    vend.Restock();
+                }
+            }
+        }
+    }
+}
